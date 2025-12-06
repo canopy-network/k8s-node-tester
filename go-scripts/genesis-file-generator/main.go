44703875b94aac88422b359b13653d2e7b8069bd
Pablo <pablojocampogo@gmail.com>
2025-12-05 23:50:57 -0400

    initial version of create script

---
+++ go-scripts/genesis-file-generator/main.go
@@ -0,0 +1,494 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/canopy-network/canopy/fsm"
+	"github.com/canopy-network/canopy/lib"
+	"github.com/canopy-network/canopy/lib/crypto"
+	"github.com/launchdarkly/go-jsonstream/v3/jwriter"
+)
+
+var (
+	defaultConfig = &lib.Config{
+		MainConfig: lib.MainConfig{
+			LogLevel: "debug",
+			ChainId:  1,
+			RootChain: []lib.RootChain{
+				{
+					ChainId: 1,
+					Url:     "http://node-1:50002",
+				},
+			},
+			RunVDF: true,
+		},
+		RPCConfig: lib.RPCConfig{
+			WalletPort:   "50000",
+			ExplorerPort: "50001",
+			RPCPort:      "50002",
+			AdminPort:    "50003",
+			RPCUrl:       "http://localhost:50002",
+			AdminRPCUrl:  "http://localhost:50003",
+			TimeoutS:     3,
+		},
+		StoreConfig: lib.StoreConfig{
+			DataDirPath: "/root/.canopy",
+			DBName:      "canopy",
+			InMemory:    false,
+		},
+		P2PConfig: lib.P2PConfig{
+			NetworkID:       1,
+			ListenAddress:   "0.0.0.0:9001",
+			ExternalAddress: "node-1",
+			MaxInbound:      21,
+			MaxOutbound:     7,
+			TrustedPeerIDs:  nil,
+			DialPeers:       []string{},
+			BannedPeerIDs:   nil,
+			BannedIPs:       nil,
+		},
+		ConsensusConfig: lib.ConsensusConfig{
+			ElectionTimeoutMS:       2000,
+			ElectionVoteTimeoutMS:   3000,
+			ProposeTimeoutMS:        3000,
+			ProposeVoteTimeoutMS:    2000,
+			PrecommitTimeoutMS:      2000,
+			PrecommitVoteTimeoutMS:  2000,
+			CommitTimeoutMS:         6000,
+			RoundInterruptTimeoutMS: 2000,
+		},
+		MempoolConfig: lib.MempoolConfig{
+			MaxTotalBytes:       1000000,
+			MaxTransactionCount: 5000,
+			IndividualMaxTxSize: 4000,
+			DropPercentage:      35,
+		},
+		MetricsConfig: lib.MetricsConfig{
+			MetricsEnabled:    true,
+			PrometheusAddress: "0.0.0.0:9090",
+		},
+	}
+	nickNames = make(chan string, 1000)
+)
+
+const (
+	validatorNick = "validator"
+	delegatorNick = "delegator"
+	accountNick   = "account"
+)
+
+func logData() {
+	var accounts, validators, delegators int32
+
+	go func() {
+		for nickname := range nickNames {
+			switch nickname {
+			case accountNick:
+				atomic.AddInt32(&accounts, 1)
+			case validatorNick:
+				atomic.AddInt32(&validators, 1)
+			case delegatorNick:
+				atomic.AddInt32(&delegators, 1)
+			default:
+				fmt.Println("Unknown data type received:", nickname)
+			}
+		}
+	}()
+
+	go func() {
+		ticker := time.NewTicker(2 * time.Second)
+
+		for range ticker.C {
+			fmt.Printf("Accounts: %d, Validators: %d, Delegators: %d\n",
+				atomic.LoadInt32(&accounts),
+				atomic.LoadInt32(&validators),
+				atomic.LoadInt32(&delegators),
+			)
+		}
+	}()
+}
+
+type IndividualFile struct {
+	ValidatorKey string
+	Config       *lib.Config
+	Keystore     *crypto.Keystore
+	Nick         string
+}
+
+type IndividualFiles struct {
+	Files []*IndividualFile
+}
+
+func mustCreateKey() crypto.PrivateKeyI {
+	pk, err := crypto.NewBLS12381PrivateKey()
+	if err != nil {
+		panic(err)
+	}
+
+	return pk
+}
+
+func mustUpdateKeystore(privateKey []byte, nickName, password string, keystore *crypto.Keystore) {
+	_, err := keystore.ImportRaw(privateKey, password, crypto.ImportRawOpts{
+		Nickname: nickName,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// addAccounts concurrently creates keys and accounts
+func addAccounts(accounts int, wg *sync.WaitGroup, semaphoreChan chan struct{}, accountChan chan *fsm.Account) {
+	for i := range accounts {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			semaphoreChan <- struct{}{}
+			defer func() { <-semaphoreChan }()
+
+			addrStr := fmt.Sprintf("%020x", i)
+
+			// fmt.Printf("Creating key for: %s \n", nick)
+
+			accountChan <- &fsm.Account{
+				Address: []byte(addrStr),
+				Amount:  1000000,
+			}
+			nickNames <- accountNick
+		}(i)
+	}
+}
+
+// addValidators concurrently creates validators and optional config
+func addValidators(validators int, isDelegate, multiNode bool, nickPrefix, password, customRootChainURL, customExternalAddress string,
+	files *IndividualFiles, gsync *sync.Mutex, wg *sync.WaitGroup, semaphoreChan chan struct{},
+	accountChan chan *fsm.Account, validatorChan chan *fsm.Validator) {
+
+	for i := range validators {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			semaphoreChan <- struct{}{}
+			defer func() { <-semaphoreChan }()
+
+			stakedAmount := 0
+			if multiNode || (i == 0 && !isDelegate) {
+				stakedAmount = 1000000000
+			}
+			nick := fmt.Sprintf("%s-%d", nickPrefix, i)
+			pk := mustCreateKey()
+			// fmt.Printf("Creating key for: %s \n", nick)
+
+			var configCopy *lib.Config
+			keystore := &crypto.Keystore{
+				AddressMap:  make(map[string]*crypto.EncryptedPrivateKey, 1),
+				NicknameMap: make(map[string]string, 1),
+			}
+			if (multiNode || i == 0) && !isDelegate {
+				config := *defaultConfig
+				if customRootChainURL != "" {
+					config.RootChain[0].Url = customRootChainURL
+				} else {
+					config.RootChain[0].Url = fmt.Sprintf("http://%s:50002", nick)
+				}
+				if customExternalAddress != "" {
+					config.ExternalAddress = customExternalAddress
+				} else {
+					config.ExternalAddress = nick
+				}
+				configCopy = &config
+				mustUpdateKeystore(pk.Bytes(), nick, password, keystore)
+			}
+
+			validatorChan <- &fsm.Validator{
+				Address:      pk.PublicKey().Address().Bytes(),
+				PublicKey:    pk.PublicKey().Bytes(),
+				Committees:   []uint64{1},
+				NetAddress:   fmt.Sprintf("tcp://%s", nick),
+				StakedAmount: uint64(stakedAmount),
+				Output:       pk.PublicKey().Address().Bytes(),
+				Delegate:     isDelegate,
+			}
+
+			accountChan <- &fsm.Account{
+				Address: pk.PublicKey().Address().Bytes(),
+				Amount:  1000000,
+			}
+
+			if configCopy != nil {
+				gsync.Lock()
+				files.Files = append(files.Files, &IndividualFile{
+					Config:       configCopy,
+					ValidatorKey: pk.String(),
+					Nick:         nick,
+					Keystore:     keystore,
+				})
+				gsync.Unlock()
+			}
+			if isDelegate {
+				nickNames <- delegatorNick
+			} else {
+				nickNames <- validatorNick
+			}
+		}(i)
+	}
+}
+
+func mustSetDirectory(dir string) {
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustDeleteInDirectory(dir string) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, entry := range entries {
+		err := os.RemoveAll(filepath.Join(dir, entry.Name()))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func mustSaveAsJSON(filename string, data any) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	err = encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func accountsWriter(accountLen int, wg *sync.WaitGroup, accountChan chan *fsm.Account) {
+	defer wg.Done()
+
+	accountsFile, err := os.Create(".config/accounts.json")
+	if err != nil {
+		panic(err)
+	}
+	defer accountsFile.Close()
+
+	writer := jwriter.NewStreamingWriter(accountsFile, 1024)
+
+	fmt.Println("Starting to write accounts!")
+
+	arr := writer.Array()
+	for range accountLen {
+		account := <-accountChan
+		accountObj := writer.Object()
+		accountObj.Name("address").String(hex.EncodeToString(account.Address))
+		accountObj.Name("amount").Int(int(account.Amount))
+		accountObj.End()
+	}
+	arr.End()
+
+	if err := writer.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+func genesisWriter(multiNode bool, validatorLen int, wg, accountsWG *sync.WaitGroup, validatorChan chan *fsm.Validator) {
+	defer wg.Done()
+
+	genesisFile, err := os.Create(".config/genesis.json")
+	if err != nil {
+		panic(err)
+	}
+	defer genesisFile.Close()
+
+	writer := jwriter.NewStreamingWriter(genesisFile, 1024)
+
+	obj := writer.Object()
+	obj.Name("time").String("2024-12-14 20:10:52")
+
+	fmt.Println("Starting to write validators!")
+
+	obj.Name("validators")
+	arr := writer.Array()
+	for range validatorLen {
+		validator := <-validatorChan
+		validatorObj := writer.Object()
+		validatorObj.Name("address").String(hex.EncodeToString(validator.Address))
+		validatorObj.Name("publicKey").String(hex.EncodeToString(validator.PublicKey))
+		validatorObj.Name("committees")
+		cArr := writer.Array()
+		for _, committee := range validator.Committees {
+			writer.Int(int(committee))
+		}
+		cArr.End()
+		validatorObj.Name("netAddress").String(validator.NetAddress)
+		validatorObj.Name("stakedAmount").Int(int(validator.StakedAmount))
+		validatorObj.Name("output").String(hex.EncodeToString(validator.Output))
+		validatorObj.Name("delegate").Bool(validator.Delegate)
+		validatorObj.End()
+	}
+	arr.End()
+
+	accountsWG.Wait()
+	fmt.Println("Accounts arrived to main thread!")
+	rawAccounts, err := os.ReadFile(".config/accounts.json")
+	if err != nil {
+		panic(err)
+	}
+	obj.Name("accounts").Raw(rawAccounts)
+
+	nonSignWindow := 10
+	maxNonSign := 4
+	if !multiNode {
+		maxNonSign = math.MaxInt64
+		nonSignWindow = math.MaxInt64
+	}
+
+	remainingFields := map[string]interface{}{
+		"params": &fsm.Params{
+			Consensus: &fsm.ConsensusParams{
+				BlockSize:       1000000,
+				ProtocolVersion: "1/0",
+				RootChainId:     1,
+				Retired:         0,
+			},
+			Validator: &fsm.ValidatorParams{
+				UnstakingBlocks:                    2,
+				MaxPauseBlocks:                     4380,
+				DoubleSignSlashPercentage:          10,
+				NonSignSlashPercentage:             1,
+				MaxNonSign:                         uint64(maxNonSign),
+				NonSignWindow:                      uint64(nonSignWindow),
+				MaxCommittees:                      15,
+				MaxCommitteeSize:                   100,
+				EarlyWithdrawalPenalty:             20,
+				DelegateUnstakingBlocks:            2,
+				MinimumOrderSize:                   1000,
+				StakePercentForSubsidizedCommittee: 33,
+				MaxSlashPerCommittee:               15,
+				DelegateRewardPercentage:           10,
+				BuyDeadlineBlocks:                  15,
+				LockOrderFeeMultiplier:             2,
+			},
+			Fee: &fsm.FeeParams{
+				SendFee:            10000,
+				StakeFee:           10000,
+				EditStakeFee:       10000,
+				UnstakeFee:         10000,
+				PauseFee:           10000,
+				UnpauseFee:         10000,
+				ChangeParameterFee: 10000,
+				DaoTransferFee:     10000,
+				SubsidyFee:         10000,
+				CreateOrderFee:     10000,
+				EditOrderFee:       10000,
+				DeleteOrderFee:     10000,
+			},
+			Governance: &fsm.GovernanceParams{
+				DaoRewardPercentage: 10,
+			},
+		},
+	}
+
+	for key, value := range remainingFields {
+		obj.Name(key)
+		data, err := json.Marshal(value)
+		if err != nil {
+			panic(err)
+		}
+		writer.Raw(json.RawMessage(data))
+	}
+
+	obj.End()
+
+	if err := writer.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	fmt.Println("Deleting old files!")
+
+	mustSetDirectory(".config")
+	mustDeleteInDirectory(".config")
+
+	fmt.Println("Creating new files!")
+
+	var (
+		delegators            = flag.Int("delegators", 10, "Number of delegators")
+		validators            = flag.Int("validators", 5, "Number of validators")
+		accounts              = flag.Int("accounts", 100, "Number of accounts")
+		password              = flag.String("password", "pablito", "Password for keystore")
+		multiNode             = flag.Bool("multiNode", false, "Flag to create config for multiples nodes or not")
+		concurrency           = flag.Int64("concurrency", 100, "Concurrency of the processes")
+		buffer                = flag.Int64("buffer", 1000, "Buffer of validators and accounts to be saved while waiting processing")
+		customRootChainURL    = flag.String("root_chain_url", "", "Custom name for root chain url")
+		customExternalAddress = flag.String("external_address", "", "Custom name for external address")
+	)
+	flag.Parse()
+
+	if *multiNode && (*customRootChainURL != "" || *customExternalAddress != "") {
+		panic("Custom root chain url and/or external address can just be used on not multi node")
+	}
+
+	acountsLen := *delegators + *validators + *accounts
+	validatorsLen := *delegators + *validators
+
+	accountChan := make(chan *fsm.Account, *buffer)
+	validatorChan := make(chan *fsm.Validator, *buffer)
+
+	logData()
+
+	var genesisWG, accountsWG sync.WaitGroup
+	genesisWG.Add(1)
+	accountsWG.Add(1)
+	go genesisWriter(*multiNode, validatorsLen, &genesisWG, &accountsWG, validatorChan)
+	go accountsWriter(acountsLen, &accountsWG, accountChan)
+
+	files := &IndividualFiles{}
+	semaphoreChan := make(chan struct{}, *concurrency)
+	var gsync sync.Mutex
+	var wg sync.WaitGroup
+	addValidators(*validators, false, *multiNode, "validator", *password, *customRootChainURL, *customExternalAddress, files, &gsync, &wg, semaphoreChan, accountChan, validatorChan)
+	addValidators(*delegators, true, *multiNode, "delegator", *password, *customRootChainURL, *customExternalAddress, files, &gsync, &wg, semaphoreChan, accountChan, validatorChan)
+	addAccounts(*accounts, &wg, semaphoreChan, accountChan)
+
+	wg.Wait()
+	genesisWG.Wait()
+
+	for _, file := range files.Files {
+		nodePath := fmt.Sprintf(".config/%s", file.Nick)
+
+		mustSetDirectory(nodePath)
+
+		input, err := os.ReadFile(".config/genesis.json")
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.WriteFile(fmt.Sprintf("%s/genesis.json", nodePath), input, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		mustSaveAsJSON(fmt.Sprintf("%s/keystore.json", nodePath), file.Keystore)
+		mustSaveAsJSON(fmt.Sprintf("%s/config.json", nodePath), file.Config)
+		mustSaveAsJSON(fmt.Sprintf("%s/validator_key.json", nodePath), file.ValidatorKey)
+	}
+}
--- go-scripts/go.mod
+++ go-scripts/go.mod
@@ -1,3 +1,69 @@
 module github.com/canopy-network/k8s-node-tester/go-scripts
 
 go 1.25.3
+
+require (
+	github.com/canopy-network/canopy v0.1.14-snapshot
+	github.com/launchdarkly/go-jsonstream/v3 v3.1.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/DataDog/zstd v1.5.7 // indirect
+	github.com/RaduBerinde/axisds v0.0.0-20250419182453-5135a0650657 // indirect
+	github.com/RaduBerinde/btreemap v0.0.0-20250419232817-bf0d809ae648 // indirect
+	github.com/alecthomas/units v0.0.0-20240927000941-0f3dac36c52b // indirect
+	github.com/allegro/bigcache/v3 v3.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cockroachdb/crlib v0.0.0-20251024155502-a2e0a212ef05 // indirect
+	github.com/cockroachdb/errors v1.12.0 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20241215232642-bb51bb14a506 // indirect
+	github.com/cockroachdb/pebble/v2 v2.1.1 // indirect
+	github.com/cockroachdb/redact v1.1.6 // indirect
+	github.com/cockroachdb/swiss v0.0.0-20250624142022-d6e517c1d961 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20250429170803-42689b6311bb // indirect
+	github.com/consensys/bavard v0.1.27 // indirect
+	github.com/consensys/gnark-crypto v0.16.0 // indirect
+	github.com/crate-crypto/go-eth-kzg v1.3.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/drand/kyber v1.3.0 // indirect
+	github.com/drand/kyber-bls12381 v0.3.1 // indirect
+	github.com/ethereum/c-kzg-4844/v2 v2.1.0 // indirect
+	github.com/ethereum/go-ethereum v1.15.11 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/fatih/color v1.17.0 // indirect
+	github.com/getsentry/sentry-go v0.36.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/kilic/bls12-381 v0.1.0 // indirect
+	github.com/klauspost/compress v1.18.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/minio/minlz v1.0.1 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20230904125328-1f23a7beb09a // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.23.2 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.67.1 // indirect
+	github.com/prometheus/procfs v0.19.1 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/supranational/blst v0.3.14 // indirect
+	go.yaml.in/yaml/v2 v2.4.3 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
+)
