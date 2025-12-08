9f99a0f4765050408b43dcbb87d7132f2b9fecf7
Roniel Valdez <roniel_valdez@outlook.com>
2025-12-08 15:55:08 +0100

    feat: makefile comments; single monitoring setup command

--- cluster/monitoring/prometheus/setup.sh
+++ cluster/monitoring/prometheus/setup.sh
@@ -8,4 +8,4 @@ helm repo update
 helm upgrade --install --create-namespace prometheus prometheus-community/kube-prometheus-stack \
   --namespace monitoring -f "${SCRIPT_DIR}/values.yml"
 
-@sed -e "s;{{DOMAIN}};$${DOMAIN};g" "${SCRIPT_DIR}/ingress-routes.yml" | kubectl apply -f -
+sed -e "s;{{DOMAIN}};${DOMAIN};g" "${SCRIPT_DIR}/ingress-routes.yml" | kubectl apply -f -
