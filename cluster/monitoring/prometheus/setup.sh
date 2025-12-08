79b341d98547c0cfd9dea47e596735051d39774b
Roniel Valdez <roniel_valdez@outlook.com>
2025-12-08 18:31:36 +0100

    infra: add canopy service monitor to prometheus setup

--- cluster/monitoring/prometheus/setup.sh
+++ cluster/monitoring/prometheus/setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# helper function to get the script directory as this could run from another directory
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# add the prometheus-community helm repository
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# install prometheus-stack
 helm upgrade --install --create-namespace prometheus prometheus-community/kube-prometheus-stack \
   --namespace monitoring -f "${SCRIPT_DIR}/values.yml"
 
+# set up an ingress route to access prometheus and grafana
 sed -e "s;{{DOMAIN}};${DOMAIN};g" "${SCRIPT_DIR}/ingress-routes.yml" | kubectl apply -f -
+
+# set up the canopy service monitor to automatically discover and monitor pods with the label "app=canopy"
+kubectl apply -f "${SCRIPT_DIR}/service-monitor.yml"
