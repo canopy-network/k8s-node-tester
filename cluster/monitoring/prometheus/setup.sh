0391aeb3dc5a5f8a968de9ca06589c42748eac73
Roniel Valdez <roniel_valdez@outlook.com>
2025-12-08 21:14:31 +0100

    feat: canopy dashboard

--- cluster/monitoring/prometheus/setup.sh
+++ cluster/monitoring/prometheus/setup.sh
@@ -7,8 +7,14 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# create a namespace for prometheus if it doesn't exist
+kubectl create namespace monitoring || true
+
+# import Grafana dashboards (ConfigMaps labeled grafana_dashboard=1) from the dashboards directory
+kubectl apply -f "${SCRIPT_DIR}/dashboards"
+
 # install prometheus-stack
-helm upgrade --install --create-namespace prometheus prometheus-community/kube-prometheus-stack \
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
   --namespace monitoring -f "${SCRIPT_DIR}/values.yml"
 
 # set up an ingress route to access prometheus and grafana
