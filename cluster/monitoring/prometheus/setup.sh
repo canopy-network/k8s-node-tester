7bb4d413bad228c5cb47c0969d6cd6b0746c4ef8
Roniel Valdez <roniel_valdez@outlook.com>
2025-12-16 17:46:00 +0100

    feat: alert manager initial setup and discord webhook

--- cluster/monitoring/prometheus/setup.sh
+++ cluster/monitoring/prometheus/setup.sh
@@ -15,7 +15,9 @@ kubectl apply -f "${SCRIPT_DIR}/dashboards"
 
 # install prometheus-stack
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
-  --namespace monitoring -f "${SCRIPT_DIR}/values.yml" --version 3.5.1
+  --namespace monitoring -f "${SCRIPT_DIR}/values.yml" \
+  --set 'grafana.alerting.contactpoints\.yml.secret.contactPoints[0].receivers[0].settings.url'="${DISCORD_WEBHOOK_URL}" \
+  --version 80.4.1
 
 # set up an ingress route to access prometheus and grafana
 sed -e "s;{{ DOMAIN }};${DOMAIN};g" "${SCRIPT_DIR}/ingress-routes.yml" | kubectl apply -f -
